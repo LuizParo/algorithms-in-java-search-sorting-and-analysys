78e0155448ccc20cb05221b2794647268a034df1
Luiz Guilherme Paro <lg_paro@hotmail.com>
2019-10-30 10:37:12 -0500

    enhancing first algorithm

--- src/main/java/br/com/alura/algoritmos/TestaMenorPreco.java
+++ src/main/java/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -3,22 +3,27 @@ package br.com.alura.algoritmos;
 public class TestaMenorPreco {
 
     public static void main(String[] args) {
-        final double[] precos = new double[5];
-        precos[0] = 1_000_000D;
-        precos[1] = 46_000D;
-        precos[2] = 16_000D;
-        precos[3] = 46_000D;
-        precos[4] = 17_000D;
+        final Produto[] produtos = {
+            new Produto("Lamborghini", 1_000_000D),
+            new Produto("Jipe", 46_000D),
+            new Produto("Brasília", 16_000D),
+            new Produto("Smart", 46_000D),
+            new Produto("Fusca", 17_000D)
+        };
 
+        int maisBarato = buscaMenor(produtos, 0, 4);
+        System.out.println("O carro " + produtos[maisBarato].getNome() + " é o mais barato, e custa: " + produtos[maisBarato].getPreco());
+    }
+
+    private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
         int maisBarato = 0;
 
-        for (int atual = 0; atual < precos.length; atual++) {
-            if (precos[atual] < precos[maisBarato]) {
+        for (int atual = inicio; atual <= termino; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
                 maisBarato = atual;
             }
         }
 
-        System.out.println(maisBarato);
-        System.out.println("O carro mais barato custa: " + precos[maisBarato]);
+        return maisBarato;
     }
 }
